6b27513dd834a3abd2d424aabb84eff55395d118
Whites11 <whites11@gmail.com>
2020-02-07 11:13:26 +0100

    Using different structs for flag names declaration and data structures holding actual flags values

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,15 +1,13 @@
 package service
 
 import (
-	"github.com/giantswarm/etcd-backup-operator/service/controller/resource"
-	"github.com/giantswarm/etcd-backup-operator/service/controller/resource/etcdbackup/storage"
 	"github.com/giantswarm/operatorkit/flag/service/kubernetes"
 )
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
 	Kubernetes kubernetes.Kubernetes
-	S3         storage.S3Uploader
-	ETCDv2     resource.ETCDv2Settings
-	ETCDv3     resource.ETCDv3Settings
+	S3         S3Uploader
+	ETCDv2     ETCDv2Settings
+	ETCDv3     ETCDv3Settings
 }
