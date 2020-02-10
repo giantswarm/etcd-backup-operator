a85a4c99497364ce189278603d54f3bfa8418571
Whites11 <whites11@gmail.com>
2020-02-10 11:59:08 +0100

    Added skeleton of the outer reconciliation loop state machine

--- service/controller/resource/etcdbackup/create.go
+++ service/controller/resource/etcdbackup/create.go
@@ -2,8 +2,77 @@ package etcdbackup
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
+
+	"github.com/giantswarm/etcd-backup-operator/service/controller/key"
+	"github.com/giantswarm/etcd-backup-operator/service/controller/resource/etcdbackup/internal/state"
 )
 
+const (
+	// States
+	GlobalBackupStatePending   = "Pending"
+	GlobalBackupStateRunning   = "Running"
+	GlobalBackupStateCompleted = "Completed"
+	GlobalBackupSateFailed     = "Failed"
+)
+
+// configureStateMachine configures and returns state machine that is driven by
+// EnsureCreated.
+func (r *Resource) configureStateMachine() {
+	sm := state.Machine{
+		GlobalBackupStatePending:   r.pendingTransition,
+		GlobalBackupStateRunning:   r.runningTransition,
+		GlobalBackupStateCompleted: r.completedTransition,
+		GlobalBackupSateFailed:     r.failedTransition,
+	}
+
+	r.stateMachine = sm
+}
+
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var newState state.State
+	var currentState state.State
+	{
+		s, err := r.getGlobalStatus(customObject)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		currentState = state.State(s)
+
+		if currentState == "" {
+			// GlobalBackupStatePending is the initial state for instance resource.
+			newState = GlobalBackupStatePending
+			r.logger.LogCtx(ctx, "level", "debug", "message", "no current state present")
+		} else {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("current state: %s", currentState))
+			newState, err = r.stateMachine.Execute(ctx, obj, currentState)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+	}
+
+	if newState != currentState {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("new state: %s", newState))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("setting resource status to '%s'", newState))
+		err = r.setGlobalStatus(customObject, string(newState))
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("set resource status to '%s'", newState))
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
+		reconciliationcanceledcontext.SetCanceled(ctx)
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "no state change")
+	}
+
 	return nil
 }
--- service/controller/resource/etcdbackup/resource.go
+++ service/controller/resource/etcdbackup/resource.go
@@ -4,6 +4,8 @@ import (
 	"github.com/giantswarm/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+
+	"github.com/giantswarm/etcd-backup-operator/service/controller/resource/etcdbackup/internal/state"
 )
 
 const (
@@ -16,17 +18,25 @@ type Config struct {
 }
 
 type Resource struct {
-	logger micrologger.Logger
+	logger       micrologger.Logger
+	k8sClient    k8sclient.Interface
+	stateMachine state.Machine
 }
 
 func New(config Config) (*Resource, error) {
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.k8sClient must not be empty", config)
+	}
 
 	r := &Resource{
-		logger: config.Logger,
+		logger:    config.Logger,
+		k8sClient: config.K8sClient,
 	}
+
+	r.configureStateMachine()
 
 	return r, nil
 }
