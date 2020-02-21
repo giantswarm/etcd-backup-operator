01c6d97fbb7f6693d8e44be49340b77f4ca9c83b
Whites11 <whites11@gmail.com>
2020-02-21 16:48:40 +0100

    Converted to single state machine with all statuses

--- service/controller/resource/etcdbackup/create_running_v3_completed.go
+++ service/controller/resource/etcdbackup/create_running_v3_completed.go
@@ -7,5 +7,6 @@ import (
 )
 
 func (r *Resource) backupRunningV3BackupCompletedTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
+	// TODO Check if any of the instances failed, and in that case set the Backup state to Failed.
 	return BackupStateCompleted, nil
 }
