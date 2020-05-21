460581daf94b03b5c3d4c5320fdd46fe7b2335d1
Paweł Kopiczko <pawel@giantswarm.io>
2020-05-21 15:15:58 +0100

    release v1.0.2 (#71)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "etcd-backup-operator"
 	source      = "https://github.com/giantswarm/etcd-backup-operator"
-	version     = "1.0.2-dev"
+	version     = "1.0.2"
 )
 
 func Description() string {
