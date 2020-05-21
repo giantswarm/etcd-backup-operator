7faeb2c537d941c9a936e50774d234d1ffb3bfae
Paweł Kopiczko <pawel@giantswarm.io>
2020-05-21 15:19:24 +0100

    project: bump to v1.0.3-dev (#72)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "etcd-backup-operator"
 	source      = "https://github.com/giantswarm/etcd-backup-operator"
-	version     = "1.0.2"
+	version     = "1.0.3-dev"
 )
 
 func Description() string {
