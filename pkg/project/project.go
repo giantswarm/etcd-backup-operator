97ce9d5d8f66b0b7a4d43fb348cdb2aa13490286
wtty-fool <wtty.fool@gmail.com>
2020-08-21 18:02:44 +0200

    bump project.go version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "etcd-backup-operator"
 	source      = "https://github.com/giantswarm/etcd-backup-operator"
-	version     = "1.0.5"
+	version     = "1.1.0"
 )
 
 func Description() string {
