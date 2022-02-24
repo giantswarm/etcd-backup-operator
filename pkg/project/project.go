eb40b78d96ee7807ae5110c278a680322ff8d72d
GitHub Action <action@github.com>
2022-02-24 11:23:55 +0000

    Bump version to 2.10.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "etcd-backup-operator"
 	source      = "https://github.com/giantswarm/etcd-backup-operator"
-	version     = "2.10.0"
+	version     = "2.10.1-dev"
 )
 
 func Description() string {
