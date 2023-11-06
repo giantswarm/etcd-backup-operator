addb4c540d043027848cadc9a911c0cf1746f0c2
taylorbot <dev@giantswarm.io>
2023-11-06 18:21:44 +0000

    Bump version to 4.4.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "etcd-backup-operator"
 	source      = "https://github.com/giantswarm/etcd-backup-operator"
-	version     = "4.4.2"
+	version     = "4.4.3-dev"
 )
 
 func Description() string {
