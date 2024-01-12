779fa5c6f580e98ae11b1ae8d3bcea10deb1d2e5
Nick Jüttner <nick@giantswarm.io>
2024-01-12 11:53:43 +0100

    skips reconciling an etcdbackup CR if there is a newer one already pending

--- service/controller/resource/etcdbackup/create_empty.go
+++ service/controller/resource/etcdbackup/create_empty.go
@@ -2,6 +2,7 @@ package etcdbackup
 
 import (
 	"context"
+	"fmt"
 
 	backupv1alpha1 "github.com/giantswarm/apiextensions-backup/api/v1alpha1"
 	"github.com/giantswarm/etcd-backup-operator/v4/service/controller/resource/etcdbackup/internal/state"
@@ -12,6 +13,11 @@ import (
 func (r *Resource) backupEmptyTransition(ctx context.Context, obj interface{}, currentState state.State) (state.State, error) {
 	var err error
 	r.logger.LogCtx(ctx, "level", "debug", "message", "no current state present")
+
+	cr, ok := obj.(*backupv1alpha1.ETCDBackup)
+	if !ok {
+		return instanceBackupStateFailed, microerror.Mask(fmt.Errorf("expected v1alpha1.EtcdBackup, got %T", obj))
+	}
 
 	backups := backupv1alpha1.ETCDBackupList{}
 	err = r.k8sClient.CtrlClient().List(ctx, &backups)
@@ -24,6 +30,11 @@ func (r *Resource) backupEmptyTransition(ctx context.Context, obj interface{}, c
 		if latestBackup.Name < backup.Name {
 			latestBackup = backup
 		}
+	}
+
+	if cr.Name != latestBackup.Name {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Backup is not the latest, skipping")
+		return instanceBackupStateSkipped, nil
 	}
 
 	if latestBackup.Status.FinishedTimestamp.IsZero() {
