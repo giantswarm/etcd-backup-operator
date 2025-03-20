2d67d346688d88c53227fa3895fc07533192949a
Nick Jüttner <nick@giantswarm.io>
2025-03-20 16:00:52 +0100

    add BackupDestination label to support multiple operator instances

--- main.go
+++ main.go
@@ -108,6 +108,7 @@ func mainE(ctx context.Context) error {
 	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CrtFile, "", "Certificate file path to use to authenticate with Kubernetes.")
 	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.KeyFile, "", "Key file path to use to authenticate with Kubernetes.")
 	daemonCommand.PersistentFlags().Bool(f.Service.SkipManagementClusterBackup, false, "Skip management cluster backup.")
+	daemonCommand.PersistentFlags().String(f.Service.BackupDestination, "", "Backup destination, primarily used for filtering when ETCDBackup CRs are watched when running multiple etcd-backup-operators, distinguishes between GiantSwarm and Customer destinations.")
 	daemonCommand.PersistentFlags().String(f.Service.S3.Bucket, "", "AWS S3 Bucket name.")
 	daemonCommand.PersistentFlags().String(f.Service.S3.Region, "", "AWS S3 Region name.")
 	daemonCommand.PersistentFlags().String(f.Service.S3.Endpoint, "", "Custom AWS S3 Endpoint.")
